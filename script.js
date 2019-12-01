7242a252ce5d72b0123679c04746a0da5fc45968
Bre Akali <34258642+13re@users.noreply.github.com>
2019-12-01 09:56:05 +0600

    20191112a_JSalert

---
+++ script.js
@@ -0,0 +1,8 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is you last name?");
+var age = prompt("What is your age?");
+
+var fullName = firstName + " " lastName;
+
+alert("Nice to meet you " + fullName + "! You are " + age + " years old!!");
+console.log("Your name is " + fullName + ". You are " + age + " years old.");
